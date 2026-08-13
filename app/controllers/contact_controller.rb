53ed116d1f2ff4e04399e2a3d72ea995041f6510
Yorrany <yorrany@yorrany.com.br>
2026-08-13 11:20:17 -0300

    Auto deploy changes

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,14 +1,17 @@
 class ContactController < ApplicationController
   def vcard
-    vcard_content = <<~VCARD
-      BEGIN:VCARD
-      VERSION:3.0
-      FN:Yorrany Braga
-      TITLE:Principal Product Designer
-      EMAIL:yorranymb@gmail.com
-      URL:https://yorrany.com.br
-      END:VCARD
-    VCARD
+    vcard_content = [
+      "BEGIN:VCARD",
+      "VERSION:3.0",
+      "N:Braga;Yorrany;;;",
+      "FN:Yorrany Braga",
+      "ORG:Yorrany Braga",
+      "TITLE:Principal Product Designer",
+      "EMAIL;type=INTERNET;type=WORK:yorranymb@gmail.com",
+      "URL:https://yorrany.com.br",
+      "END:VCARD"
+    ].join("\r\n")
+
     send_data vcard_content, filename: "yorrany_braga.vcf", type: "text/vcard", disposition: "attachment"
   end
 end
